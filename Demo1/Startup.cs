26282d33f89758a21dca63fd1a72b87ca6e5c9ce
Yarne Coppens <yarne.coppens@student.howest.be>
2018-02-19 10:08:57 +0100

    Eerste website

--- Demo1/Startup.cs
+++ Demo1/Startup.cs
@@ -25,9 +25,12 @@ namespace Demo1
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello starman!");
             });
         }
     }
